702deac97ebd80a40ca8db02458ae6a824630dc9
mroger <rene.mroger@gmail.com>
2019-08-30 22:36:34 +0000

    adding data - airlines, airports, countries

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,61 +1,64 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+import React, { useState, useEffect, useRef } from "react";
+import axios from "axios";
 import * as THREE from "three";
-import './App.css';
+import "./App.css";
 
-const App = (props) => {
-
+const App = props => {
   const [message, setMessage] = useState({
-    message: 'Click the button to load data!'
+    message: "Click the button to load data!"
   });
-
+  const mount = useRef(null);
+  const [isAnimating, setAnimating] = useState(true);
+  const controls = useRef(null);
 
   useEffect(() => {
     // === THREE.JS CODE START ===
+    console.log(mount);
     var scene = new THREE.Scene();
-    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    );
     var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
-    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
-    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-    var cube = new THREE.Mesh( geometry, material );
-    scene.add( cube );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+    var geometry = new THREE.BoxGeometry(1, 1, 1);
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+    scene.add(cube);
     camera.position.z = 5;
-    var animate = function () {
-      requestAnimationFrame( animate );
+    var animate = function() {
+      requestAnimationFrame(animate);
       cube.rotation.x += 0.01;
       cube.rotation.y += 0.01;
-      renderer.render( scene, camera );
+      renderer.render(scene, camera);
     };
     animate();
     // === THREE.JS EXAMPLE CODE END ===
-  }, [])
-
+  }, []);
 
   const fetchData = () => {
-    axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
-    .then((response) => {
-      // handle success
-      console.log(response.data) 
+    axios
+      .get("/api/data") // You can simply make your requests to "/api/whatever you want"
+      .then(response => {
+        // handle success
+        console.log(response.data);
 
-      console.log(response.data.message) 
-      setMessage({
-        message: response.data.message
+        console.log(response.data.message);
+        setMessage({
+          message: response.data.message
+        });
       });
-    }) 
-  }
+  };
 
-
-    return (
-      <div className="App">
-        <h1>{ message.message }</h1>
-        <button onClick={fetchData} >
-          Fetch Data
-        </button>        
-      </div>
-    );
-  
-}
+  return (
+    <div className="App">
+      <h1>{message.message}</h1>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  );
+};
 
 export default App;
