f747ab6fd434790e1c06f52b789362387d0284e8
Scott Appleton <Scottappleton09@gmail.com>
2019-08-30 00:12:19 +0000

    added threejs to App. converted App to functional component

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,38 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import * as THREE from "three";
 import './App.css';
 
-class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      message: 'Click the button to load data!'
-    }
-  }
+const App = (props) => {
 
-  fetchData = () => {
+  const [message, setMessage] = useState({
+    message: 'Click the button to load data!'
+  });
+
+
+  useEffect(() => {
+    // === THREE.JS CODE START ===
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    var geometry = new THREE.BoxGeometry( 1, 1, 1 );
+    var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+    var cube = new THREE.Mesh( geometry, material );
+    scene.add( cube );
+    camera.position.z = 5;
+    var animate = function () {
+      requestAnimationFrame( animate );
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
+      renderer.render( scene, camera );
+    };
+    animate();
+    // === THREE.JS EXAMPLE CODE END ===
+  }, [])
+
+
+  const fetchData = () => {
     axios.get('/api/data') // You can simply make your requests to "/api/whatever you want"
     .then((response) => {
       // handle success
-      console.log(response.data) // The entire response from the Rails API
+      console.log(response.data) 
 
-      console.log(response.data.message) // Just the message
-      this.setState({
+      console.log(response.data.message) 
+      setMessage({
         message: response.data.message
       });
     }) 
   }
 
-  render() {
+
     return (
       <div className="App">
-        <h1>{ this.state.message }</h1>
-        <button onClick={this.fetchData} >
+        <h1>{ message.message }</h1>
+        <button onClick={fetchData} >
           Fetch Data
         </button>        
       </div>
     );
-  }
+  
 }
 
 export default App;
