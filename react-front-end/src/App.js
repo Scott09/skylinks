4453e827c2d943c55816e337b16f19a1b358330c
mroger <rene.mroger@gmail.com>
2019-09-05 22:06:45 +0000

    data

--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -1,59 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
 import "./App.css";
 import ThreeContainer from "./threejs/ThreeContainer";
-import canvas from "./threejs/SceneManager";
+import YVR_routes from "./YVR_routes.json";
 
 const App = props => {
-  const { removeEntity, printScene, addEntity, toggleRemove } = canvas();
+  const [clearToggle, setClearToggle] = useState(false);
+
   const fetchData = () => {
-    removeEntity();
     axios.get("/api/airports").then(response => {
       console.log(response.data);
-      // setCity(response.data);
+      // setCity(response.data);props
     });
   };
-  const _printScene = () => {
-    printScene();
-  };
   const _addEntity = () => {
-    addEntity();
+    setClearToggle(false);
   };
 
   const _removeEntity = () => {
-    let random = Math.random();
+    setClearToggle(true);
   };
-  // document.addEventListener("mousedown", onDocumentMouseDown);
-  // // Set up the controls
-  //   let controls = new OrbitControls(camera);
-  //   controls.minPolarAngle = 1.52;
-  //   controls.maxPolarAngle = 1.52;
-  //   controls.minAzimuthAngle = -Infinity;
-  //   controls.maxAzimuthAngle = Infinity;
-  //   controls.rotateSpeed = 0.3;
-  //   controls.zoomSpeed = 0.5;
-  //   controls.maxDistance = 50;
-  //   controls.minDistance = 6;
-  //   controls.enablePan = false;
-  //   // controls.autoRotate = true;
-  //   // controls.autoRotateSpeed = 0.3;
-  //   controls.update();
-
-  //   let animate = function() {
-  //     requestAnimationFrame(animate);
-  //     CLOUDS.rotation.y += 0.0001;
-  //     controls.update();
-  //     renderer.render(scene, camera);
-  //   };
-  //   animate();
-  // }, []);
 
   return (
     <>
       <button onClick={_addEntity}> add entity </button>
       <button onClick={_removeEntity}> remove entity </button>
-      <button onClick={_printScene}> print Scene </button>
-      <ThreeContainer clear={random} />
+      <ThreeContainer clear={clearToggle} newAirport={YVR_routes} />
     </>
   );
 };
