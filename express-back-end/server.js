f42f02b73a01f12922db1c353dad83d04f76558f
Haibin Zhu <ssspiderzzz@gmail.com>
2019-09-05 19:04:39 -0700

    test query from server, for YYZ airport

--- express-back-end/server.js
+++ express-back-end/server.js
@@ -2,22 +2,71 @@ const Express = require("express");
 const App = Express();
 const BodyParser = require("body-parser");
 const PORT = 8080;
+const { Pool } = require("pg");
+require("dotenv").config();
+
+const pool = new Pool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
+});
+
+pool.connect((error, client) => {
+  console.log(process.env.DB_HOST);
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("connected");
+  }
+});
 
 // Express Configuration
 App.use(BodyParser.urlencoded({ extended: false }));
 App.use(Express.static("public"));
 
 // Sample GET route
-App.get("/api/data", (req, res) =>
+App.get("/api/data", (req, res) => {
   res.json({
     message: "Seems to work!"
-  })
-);
-
-App.get("/api/airports ", (req, res) => {
-  res.json({
-    data: ""
   });
+});
+
+App.get("/api/airports", (req, res) => {
+  const findDepartureCoords = {
+    text: `
+    SELECT iata, latitude, longitude FROM airports 
+    WHERE iata = $1;
+    `,
+    values: ["YYZ"]
+  };
+  const findArrivalCoords = {
+    text: `
+    SELECT iata, latitude, longitude FROM airports 
+    WHERE iata IN
+    (SELECT DISTINCT arrival_iata FROM airports 
+    JOIN routes ON routes.departure_iata = airports.iata 
+    WHERE airports.iata = $1);
+    `,
+    values: ["YYZ"]
+  };
+
+  pool
+    .query(findDepartureCoords)
+    .then(coords => {
+      pool.query(findArrivalCoords).then(arrival => {
+        console.log(coords.rows);
+        console.log(arrival.rows);
+        res.json({
+          airport: coords.rows[0],
+          arrival: arrival.rows
+        });
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
 });
 
 App.listen(PORT, () => {
--- react-front-end/src/App.js
+++ react-front-end/src/App.js
@@ -6,11 +6,12 @@ import YVR_routes from "./YVR_routes.json";
 
 const App = props => {
   const [clearToggle, setClearToggle] = useState(false);
+  const [airport, setAirport] = useState("");
 
   const fetchData = () => {
     axios.get("/api/airports").then(response => {
-      console.log(response.data);
-      // setCity(response.data);props
+      console.log({ response });
+      setAirport(response);
     });
   };
   const _addEntity = () => {
@@ -23,6 +24,7 @@ const App = props => {
 
   return (
     <>
+      <button onClick={fetchData}> Toronto Airport </button>
       <button onClick={_addEntity}> add entity </button>
       <button onClick={_removeEntity}> remove entity </button>
       <ThreeContainer clear={clearToggle} newAirport={YVR_routes} />
