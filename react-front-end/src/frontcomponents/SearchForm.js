5d6d5fdd6be170d6ee33f4c8158b366e82b177ea
Scott Appleton <Scottappleton09@gmail.com>
2019-09-06 00:06:38 +0000

    add front end FlightList component

---
+++ react-front-end/src/frontcomponents/SearchForm.js
@@ -0,0 +1,54 @@
+import React , {useState} from 'react';
+import Container from '@material-ui/core/Container';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+  },
+  dense: {
+    marginTop: theme.spacing(2),
+  },
+  menu: {
+    width: 200,
+  },
+}));
+
+
+
+const SearchForm = (props) => {
+
+  const classes = useStyles();
+  const [departure, setDeparture] = useState('');
+
+
+  
+
+  return (
+    <>
+    <form>
+      <label>
+        Departure Airport:
+        <TextField
+          id="filled-name"
+          label="Departure"
+          className={classes.textField}
+          value={departure}
+          onChange={handleChange('name')}
+          margin="normal"
+          variant="filled"
+        />
+      </label>
+    </form>
+    </>
+  );
+}
+
+export default SearchForm;
