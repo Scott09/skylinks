d827c57ef8cc9bea6fff5a20fd9eab3aa8105a96
Haibin Zhu <spiderz@Haibins-MacBook-Air.local>
2019-09-07 16:14:02 -0700

    Routelist component created

---
+++ react-front-end/src/frontcomponents/RouteList.js
@@ -0,0 +1,58 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ListItemText from "@material-ui/core/ListItemText";
+import PlaneLogo from "./flying-airliner.svg";
+import Arrow from "./right-arrow.svg";
+import "./RouteList.css";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    width: "100%",
+    height: 500,
+    maxWidth: 250,
+    backgroundColor: "white",
+    border: "4px outset #1C6EA4",
+    borderRadius: "8px",
+    boxShadow: "20px 16px 44px -16px rgba(101,101,102,1)",
+    position: "absolute",
+    top: 110,
+    right: 10,
+    opacity: 0.5
+  }
+}));
+
+export default function RouteList(props) {
+  const classes = useStyles();
+
+  return (
+    <React.Fragment>
+      {props.routes && (
+        <div className={classes.root}>
+          <List component="nav" aria-label="main mailbox folders">
+            <ListItemText
+              primary={`Departure from: (${props.routes.departure.fs}) ${props.routes.departure.name}`}
+            />
+            {props.routes.arrival.map(arrival => {
+              return (
+                <>
+                  <ListItem className={classes.mainContainer} button>
+                    <ListItemIcon>
+                      <img src={PlaneLogo} height="44" width="44" />
+                    </ListItemIcon>
+                    <ListItemText>
+                      <img src={Arrow} height="12" width="12" /> To: (
+                      {arrival.fs}) {arrival.name}
+                    </ListItemText>
+                  </ListItem>
+                </>
+              );
+            })}
+          </List>
+        </div>
+      )}
+    </React.Fragment>
+  );
+}
