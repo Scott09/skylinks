53046878d0d4b62add6c2bd7d5d83e8bd9c5c4c0
mroger <rene.mroger@gmail.com>
2019-09-08 01:19:06 +0000

    file creation

---
+++ react-front-end/src/threejs/components/realFlightRoutes.js
@@ -0,0 +1,75 @@
+import * as THREE from "three";
+
+import { getSplineFromCoords } from "../helpers/curve";
+import { CURVE_SEGMENTS } from "../helpers/constants";
+import makePlaneInstance from "./plane";
+
+export default (scene, airport) => {
+  if (airport) {
+    const group = new THREE.Group();
+    /**
+     * returns a filtedred list if airports
+     * @param  {} name_of_airport, the of the airport
+     * @param  {} list_of_airports
+     */
+
+    function makeLineInstance(
+      departure_airport,
+      arrival_airport,
+      curve_material
+    ) {
+      const startCoor = [
+        parseFloat(departure_airport.latitude),
+        parseFloat(departure_airport.longitude)
+      ];
+      const endCoor = [
+        parseFloat(arrival_airport.latitude),
+        parseFloat(arrival_airport.longitude)
+      ];
+      const { spline } = getSplineFromCoords(startCoor, endCoor);
+      const points = spline.getPoints(CURVE_SEGMENTS);
+      const curve_geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+      const plane = makePlaneInstance(spline);
+
+      const curvedLine = new THREE.Line(curve_geometry, curve_material);
+      curvedLine.name = `line_${departure_airport.fs}_${arrival_airport.fs}`;
+      curvedLine.departure_fs = departure_airport.fs;
+      curvedLine.arrival_fs = arrival_airport.fs;
+      curvedLine.add(plane);
+
+      return curvedLine;
+    }
+
+    function routesPerAirport(airport) {
+      for (const arrival of airport.arrival) {
+        const departure_airport = airport.departure;
+        const arrival_airport = arrival;
+        const curve_material = new THREE.MeshBasicMaterial({
+          blending: THREE.AdditiveBlending,
+          opacity: 0.7,
+          transparent: true,
+          color: 0xe85d33
+        });
+        const line = makeLineInstance(
+          departure_airport,
+          arrival_airport,
+          curve_material
+        );
+        if (line) {
+          group.add(line);
+        }
+      }
+    }
+
+    routesPerAirport(airport);
+    group.name = "realRoute";
+    scene.add(group);
+
+    function update() {}
+
+    return {
+      update
+    };
+  }
+};
