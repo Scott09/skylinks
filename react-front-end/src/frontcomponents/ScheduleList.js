3cafeaa7535b8e6b46239434fc41596500e61f8a
Haibin Zhu <ssspiderzzz@gmail.com>
2019-09-08 16:36:49 -0700

    select routes, send live api request

--- react-front-end/src/frontcomponents/ScheduleList.js
+++ react-front-end/src/frontcomponents/ScheduleList.js
@@ -7,6 +7,7 @@ import ListItemText from "@material-ui/core/ListItemText";
 import PlaneLogo from "./flying-airliner.svg";
 import AirportTower from "./airport.png";
 import Arrow from "./right-arrow.svg";
+import "./ScheduleList.css";
 
 const useStyles = makeStyles(theme => ({
   root: {
@@ -19,25 +20,42 @@ const useStyles = makeStyles(theme => ({
     boxShadow: "20px 16px 44px -16px rgba(101,101,102,1)",
     position: "absolute",
     top: 60,
-    right: 10,
+    left: 10,
     opacity: 0.5,
     overflow: "auto"
   }
 }));
 
-export default function RouteList(props) {
+const now = new Date();
+const y = now.getYear() + 1900;
+const m = now.getMonth() + 1;
+const d = now.getDate();
+
+function beautyTime(time) {
+  let ampm = "";
+  let temp = time.split("T")[1];
+  temp.split(":");
+  console.log(temp);
+  let hour = parseInt(temp.split(":")[0]);
+  let min = temp.split(":")[1];
+  if (hour < 12) {
+    ampm = " a.m.";
+  } else {
+    ampm = " p.m.";
+  }
+  return hour + ":" + min + ampm;
+}
+
+export default function ScheduleList(props) {
   const classes = useStyles();
 
   return (
     <React.Fragment>
-      {props.newDeparture && props.newArrival[0] && (
+      {console.log(props.newSchedule)}
+      {props.newSchedule && (
         <div id="list_main" className={classes.root}>
           <List component="nav" aria-label="main mailbox folders">
-            <ListItem
-              className={classes.mainContainer}
-              onClick={() => props.getDepartures(props.newDeparture.fs)}
-              button
-            >
+            <ListItem className={classes.mainContainer} button>
               <ListItemIcon>
                 <img
                   alt="AirportTower"
@@ -47,28 +65,19 @@ export default function RouteList(props) {
                 />
               </ListItemIcon>
               <ListItemText
-                primary={`Departure from: (${props.newDeparture.fs}) ${props.newDeparture.name}`}
+                primary={`Flight Schedule from ${props.newDeparture.name} to ${props.newArrival[0].name} on ${y}/${m}/${d}`}
               />
             </ListItem>
-            {props.newArrival.map(arrival => {
+            {props.newSchedule.scheduledFlights.map(schedule => {
               return (
                 <>
-                  <ListItem
-                    className={classes.mainContainer}
-                    onClick={() => props.onSelect(arrival)}
-                    button
-                  >
-                    <ListItemIcon>
-                      <img
-                        alt="PlaneLogo"
-                        src={PlaneLogo}
-                        height="33"
-                        width="33"
-                      />
-                    </ListItemIcon>
-                    <ListItemText>
-                      <img alt="Arrow" src={Arrow} height="12" width="12" /> To:
-                      ({arrival.fs}) <br></br> {arrival.name}
+                  <ListItem className={classes.mainContainer} button>
+                    <ListItemText id="scheduleText">
+                      Flight Number: {schedule.carrierFsCode}
+                      {schedule.flightNumber} <br></br>
+                      Departure: {beautyTime(schedule.departureTime)}
+                      <br></br>
+                      Arrival: {beautyTime(schedule.arrivalTime)}
                     </ListItemText>
                   </ListItem>
                 </>
