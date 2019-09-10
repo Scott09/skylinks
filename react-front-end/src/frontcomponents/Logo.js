3c2a650497f561a29fa9d2a5299d7e075d3fe84f
mroger <rene.mroger@gmail.com>
2019-09-10 00:50:12 +0000

    clean up + express data

--- react-front-end/src/frontcomponents/Logo.js
+++ react-front-end/src/frontcomponents/Logo.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import './Logo.css';
-import airplane from './airplane.png';
-import globe from './globe.png';
+import React from "react";
+import "./Logo.css";
+import airplane from "./airplane.png";
 
-const Logo = (props) => {
+const Logo = props => {
   return (
     <div id="logo">
-     <p><img className="plane" src={airplane} alt="plane"></img> SKYLINKS</p>
-     
+      <p>
+        <img className="plane" src={airplane} alt="plane"></img> SKYLINKS
+      </p>
     </div>
   );
-}
+};
 
 export default Logo;
